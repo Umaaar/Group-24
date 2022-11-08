8c50729a2888c88b86e861a134ea9bfe4503410b
sukhpal0525 <sukhpal.shergill1103@gmail.com>
2022-11-08 09:39:09 +0000

    Created a page header asset

---
+++ Website/resources/views/assets/common/header.blade.php
@@ -0,0 +1,11 @@
+<div class="bg-light">
+  <div class="p-lg-5 col-md-5 mx-auto">
+    <div class="text-center">
+        <h1>Men's Clothing</h1>
+        <p class="text-muted lead">Check out our selection on Men's clothing on this page. Looking for something else? Check out our other categories below...</p>
+          <a href="#" class="btn btn-outline-secondary my-2">Shoes</a>
+          <a href="#" class="btn btn-outline-secondary my-2">Accessories</a>
+          <a href="#" class="btn btn-outline-secondary my-2">Bags</a>
+        </div>
+      </div>
+    </div>
