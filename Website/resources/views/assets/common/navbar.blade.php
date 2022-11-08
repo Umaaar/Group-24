770161525f6137e0be21b83f5f8929703e19eb53
sukhpal0525 <sukhpal.shergill1103@gmail.com>
2022-11-08 09:31:25 +0000

    Update navbar colour & add basket icon

--- Website/resources/views/assets/common/navbar.blade.php
+++ Website/resources/views/assets/common/navbar.blade.php
@@ -18,10 +18,14 @@
           <li class="nav-item active pl-4 pr-4"><a class="nav-link" href="#"><b>DASHBOARD</b></a></li>
         </ul>
       </div>
-    <!-- <div class="vertical"></div> -->
-    <div class="row">
-      <button type="button" class="btn btn-primary btn-sm mr-2">Login</button>
-      <button type="button" class="btn btn-secondary btn-sm mr-2">Register</button>
-    </div>
-  </nav>
-</head>
+
+      <div class="row">
+        <button type="button" class="text-black btn btn-primary btn-sm mr-2">Login</button>
+        <button type="button" class="text-black btn btn-light btn-sm mr-2">Register</button>
+
+        <a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="white" class="bi bi-cart mr-2 mb-0.5" viewBox="0 0 16 16">
+          <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+          <svg></a>
+      </div>
+    </nav>
+  </head>
