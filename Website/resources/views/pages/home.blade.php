ca80b9268094de0b1aa021e7157dcb078cc9bf6b
sukhpal0525 <102860076+sukhpal0525@users.noreply.github.com>
2022-11-15 22:34:00 +0000

    Updated home, added product image slideshow, added header.

--- Website/resources/views/pages/home.blade.php
+++ Website/resources/views/pages/home.blade.php
@@ -1,54 +1,55 @@
+<!-- Import main -->
+@include('assets.main')
+
+<!-- Import navbar -->
 @include('assets.common.navbar')
 
 <head>
-  <link href="resources/css/style.css" rel="stylesheet">
-
-  <!-- Import main -->
-  @include('assets.main')
+  <link rel="stylesheet" href="{{asset('/css/home.css')}}">
 
   <title>@yield('title')</title>
 </head>
 
 <body>
-  <div class="px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
-    <img class="img-custom" src="{{URL::asset('aston_sport_clear.png')}}" width="370" height="200">
-    <!-- <h1 class="display-6"></h1> -->
+  <div class="jumbotron my-0 bg-light">
+    <div class="container">
+      <h1 class="display-4 text-dark">Welcome</h1>
+      <div class="p-lg-1 col-md-7 border-bottom border-secondary">
+        <p class="text-dark">Aston Sport aims to provide only the best quality products with competitive prices, reliable delivery times, and unmatched customer service.</p>
+        <p class="text-dark"><u>Be sure to check out our extensive product line below:</u></p>
+      </div>
+      <p><a class="btn btn-lg btn-primary mt-4" href="/products">View products...</a></p>
+    </div>
   </div>
 
-  <!-- ##################################### -->
-  <!-- ########## START HOME BODY ########## -->
-  <!-- ##################################### -->
+  <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+    <div class="carousel-inner">
+      <div class="carousel-item d-flex justify-content-center">
+        <img class="w-25" src="{{URL::asset('aston_sport_clear.png')}}">
+      </div>
+      <div class="carousel-item d-flex justify-content-center">
+        <img class="w-25" src="{{URL::asset('aston_sport_clear.png')}}">
+      </div>
+      <div class="carousel-item d-flex justify-content-center">
+        <img class="w-25" src="{{URL::asset('aston_sport_clear.png')}}">
+      </div>
+    </div>
 
- <!-- p-md-5 p-3 m-md-3 overflow-hidden -->
-<div class="position-relative text-center bg-light">
-  <div class="col-md-4 p-lg-5 mx-auto my-5">
-    <!-- <img class="img-custom" src="{{URL::asset('product1.jpg')}}"> -->
-    <p class="lead font-weight-normal">A modern, innovative, and affordable clothing retailer with an emphasis on sustainability and ethical sourcing.</p>
-    <a class="btn btn-outline-secondary" href="/about">Find out more...</a>
+    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon"></span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+      <span class="carousel-control-next-icon"></span>
+    </a>
   </div>
-</div>
 
-<div class="position-relative text-center bg-light">
-  <div class="col-md-4 p-lg-5 mx-auto my-5">
-    <!-- <img class="img-custom" src="{{URL::asset('product1.jpg')}}"> -->
-    <p class="lead font-weight-normal">A modern, innovative, and affordable clothing retailer with an emphasis on sustainability and ethical sourcing.</p>
-    <a class="btn btn-outline-secondary" href="#">Find out more...</a>
+  <div class="col-md-4 p-lg-5 mx-auto my-1">
+    <p class="lead font-weight-normal text-dark">A modern, innovative, and affordable clothing retailer with an emphasis on sustainability and ethical sourcing.</p>
+    <a class="btn btn-outline-dark d-block" href="/about">Find out more...</a>
   </div>
-</div>
 
-<div class="position-relative text-center bg-light">
-  <div class="col-md-4 p-lg-5 mx-auto my-5">
-    <!-- <img class="img-custom" src="{{URL::asset('product1.jpg')}}"> -->
-    <p class="lead font-weight-normal">A modern, innovative, and affordable clothing retailer with an emphasis on sustainability and ethical sourcing.</p>
-    <a class="btn btn-outline-secondary" href="#">Find out more...</a>
-  </div>
-</div>
+<!-- Import footer -->
+@include('assets.common.footer')
 
-    <!-- ################################### -->
-    <!-- ########## END HOME BODY ########## -->
-    <!-- ################################### -->
-
-  <!-- Import footer -->
-  @include('assets.common.footer')
 </body>
 </html>
