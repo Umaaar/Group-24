3f362446523c20f4480740599b891607e93e962d
rabiafatima1 <115074530+rabiafatima1@users.noreply.github.com>
2022-11-15 12:12:38 +0000

    about page styling
    added the style to the about page

--- Website/resources/views/pages/about.blade.php
+++ Website/resources/views/pages/about.blade.php
@@ -4,6 +4,8 @@
   @include('assets.main')
 
   <title>@yield('title')</title>
+  
+ 
 </head>
 
 <body>
@@ -16,7 +18,20 @@
   </div>
 
   <!-- Insert Home body here -->
-
+  <link rel="stylesheet" type="text/css" href="/css/about.css" />
+    <title> About Us</title>
+<body>
+    <?php
+    inlcude("nav.php");
+    ?>
+    <div class="container">
+    <div class="box">
+<h1> About Us</h1>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
+</div>
+</div>
+</body>
+</html>
   <!-- Import footer -->
   @include('assets.common.footer')
 
