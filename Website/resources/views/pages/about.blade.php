c8e0788e5abf8e1cc7c9a0ea8cf5de410b91d551
rabiafatima1 <115074530+rabiafatima1@users.noreply.github.com>
2022-11-16 10:34:00 +0000

    code layout
    fixing the code layout

--- Website/resources/views/pages/about.blade.php
+++ Website/resources/views/pages/about.blade.php
@@ -1,34 +1,36 @@
+
 <head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <!-- Import main -->
+   @include('assets.main')
 
-  <!-- Import main -->
-  @include('assets.main')
-
-  <title>@yield('title')</title>
+<title>@yield('title')</title>
+<link rel="stylesheet" type="text/css" href="/css/about.css">
+  <title> About Us</title>
   
- 
 </head>
+
 
 <body>
 
   <!-- Import header -->
   @include('assets.common.navbar')
 
-  <div class="px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
-    <h1 class="display-6">About-Us</h1>
-  </div>
+  <!--<div class="px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+    <h1 class="display-6">About-Us</h1> 
+  </div> -->
 
-  <!-- Insert Home body here -->
-  <link rel="stylesheet" type="text/css" href="/css/about.css" />
-    <title> About Us</title>
-<body>
+  <!-- Insert About body here -->
    
-    <div class="container">
-    <div class="box">
+    <div class="boxb">
+    <div class="boxa">
 <h1> About Us</h1>
 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
 </div>
 </div>
-</body>
+
 </html>
   <!-- Import footer -->
   @include('assets.common.footer')
