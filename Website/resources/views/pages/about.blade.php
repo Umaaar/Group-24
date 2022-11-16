3000f57b768e59a4fbd838d8ad9216337e9e3fc3
rabiafatima1 <115074530+rabiafatima1@users.noreply.github.com>
2022-11-16 18:33:46 +0000

    about us css
    code css code change

--- Website/resources/views/pages/about.blade.php
+++ Website/resources/views/pages/about.blade.php
@@ -8,7 +8,7 @@
 
 <title>@yield('title')</title>
 <link rel="stylesheet" type="text/css" href="/css/about.css">
-  <title> About Us</title>
+  
   
 </head>
 
@@ -23,16 +23,13 @@
   </div> -->
 
   <!-- Insert About body here -->
-   
-    <div class="boxb">
-    <div class="boxa">
+ <div class="coontainer-about-us">  
 <h1> About Us</h1>
 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
 </div>
-</div>
-
+<div class="container-2">
   <!-- Import footer -->
   @include('assets.common.footer')
-
+  </div>
 </body>
 </html>
