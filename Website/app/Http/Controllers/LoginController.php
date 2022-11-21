f621c7e3ae568168ed12a33fdfbf17bcb3f86f43
BogdanSaf <115074233+BogdanSaf@users.noreply.github.com>
2022-11-21 15:03:37 +0000

    Loggin controller now adds information to session

--- Website/app/Http/Controllers/LoginController.php
+++ Website/app/Http/Controllers/LoginController.php
@@ -13,15 +13,26 @@ class LoginController extends Controller
     protected function login(Request $request)
     {
         
-        $credentials = $request->validate([
+        $validated = $request->validate([
             'email' => ['required', 'email'],
             'password' => ['required'],
         ]);
 
-        if(Auth::attempt($credentials)) {
-             return redirect('/');
+        if(Auth::attempt($validated)) {
+            
+            $user = DB::table('users')->where('email',$validated['email'])->first();
+
+            session(['firstname' => $user->firstName]);
+            session(['surname' => $user->surname]);
+            session(['address' => $user->address]);
+            session(['postcode' => $user->postCode]);
+            session(['gender' => $user->gender]);
+            session(['dateofbirth' =>$user->dateOfBirth]);
+
+
+            return redirect('/');
         }else{
-            return back()->withErrors(['error' => 'The email or password were incorrect!']);
+            return back()->withErrors(['error' => 'The email or password are incorrect!']);
         }
 
        
