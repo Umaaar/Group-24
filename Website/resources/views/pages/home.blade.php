ceab6d98f6635f25802353156efe0238ac1d6f4c
Hu3na <115074261+Hu3na@users.noreply.github.com>
2022-11-21 19:40:30 +0000

    update to home blade
    change to description

--- Website/resources/views/pages/home.blade.php
+++ Website/resources/views/pages/home.blade.php
@@ -54,7 +54,7 @@ if(Auth::check()){
     <div class="border-left border-secondary bg-dark mx-auto w-50 px-5 w-100"> <!-- col-md-3 -->
       <h1 class="display-5 text-light py-2">Welcome <?php echo $name ?></h1>
       <!-- <p class="display-6 my-3 text-light">Test</p> -->
-      <p class="font-weight-normal text-light">A modern, innovative, and affordable clothing retailer with an emphasis on sustainability and ethical sourcing.</p>
+      <p class="font-weight-normal text-light">A modern, innovative, and affordable clothing retailer perfect for students on a budget. We place a big emphasis on sustainability and ethical sourcing.</p>
       <a class="btn btn-secondary d-block" href="/about">Find out more...</a>
     </div>
   </div>
