1ec7da004a05c24253eedd7fc1f242a0daa47be4
Rehan Hussain <rehanhh285@gmail.com>
2022-11-22 19:35:30 +0000

    changes

--- Website/resources/views/pages/checkout.blade.php
+++ Website/resources/views/pages/checkout.blade.php
@@ -24,7 +24,7 @@
   </div>
     <!-- Insert checkout page here-->
     
-   <div class="container mt-5">
+   <div class="container mt-5 p-5 pt-2">
     <div class="row">
       <div class="col-md-7">
         <div class="card">
