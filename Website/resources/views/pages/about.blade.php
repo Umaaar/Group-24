788c97e97c5618f369b07ac93a049e1e62d6d166
rabiafatima1 <115074530+rabiafatima1@users.noreply.github.com>
2022-11-23 09:19:23 +0000

    code fix
    Fixed the code that was incorrect

--- Website/resources/views/pages/about.blade.php
+++ Website/resources/views/pages/about.blade.php
@@ -24,7 +24,7 @@
   </div> -->
 
   <!-- Insert About body here -->
-<<<<<<< HEAD
+
   <div class="coontainer-about-us">
     <div class="container-sub-about">
     <img src="{{ asset('\Website\resources\views\pages\logo.jpg') }}" alt="description of myimage" style="width:4%;">
@@ -35,21 +35,11 @@
     <div class="container-sub-about">
      
 
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-        pariatur.
+      <p>AstonDirect is an e-commerce platform for students. We sell great quality sports clothing at affordable prices perfect for all students on a tight budget. Our modern look reflects our emphasis on being ethical and sustainable durin development of our products. We are proud to be able to provide such a service and hope all you guys are as excited about AstonDirect as we are.
       </p>
     </div>
-=======
- <div class="coontainer-about-us">  
-  <div class="container-sub-about"> <img src="C:\xampp\htdocs\github\Group-24\Website\logo.png" alt=""></div>
-  <div class="container-sub-about">
-  <h1> About Us</h1>
-<p> AstonDirect is an e-commerce platform made by students for students. We sell great quality sports clothing at affordable prices perfect for all students on a tight budget. Our modern look reflects our emphasis on being ethical and sustainable durin development of our products. We are proud to be able to provide such a service and hope all you guys are as excited about AstonDirect as we are.</p>
-</div>
->>>>>>> 429e11782fd2e13ee0ea5ee54eeee8b8e5d0fc00
-  </div>
+
+
 
 
   <div class="container-2">
