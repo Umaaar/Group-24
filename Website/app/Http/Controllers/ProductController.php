708a5630f5021a0634cc75c11151e0cd6d4b23ea
Mohammad Saeed <mohumarsaeed007@gmail.com>
2022-11-26 15:49:02 +0000

    displays products from database into grid

--- Website/app/Http/Controllers/ProductController.php
+++ Website/app/Http/Controllers/ProductController.php
@@ -8,13 +8,16 @@ use App\Models\Product;
 
 class ProductController extends Controller
 {
-
+	
 	public function display_products() {
-      $all = Product::all();
+		$all = Product::all();
       return view('pages.admin.products', ['products' => $all]);
 	}
 
-	public function products_men() { return view('pages.products-men'); }
+	public function products_men() { 
+		$all = Product::all();
+		return view('pages.products-men', ['products' => $all]); 
+	}
 	public function products_women() { return view('pages.products-women'); }
 
 	public function display_addproduct_page() {
