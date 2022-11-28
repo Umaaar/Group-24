01f9552488e64ba0e19f1e4c9a5370dcc8d32d08
rabiafatima1 <115074530+rabiafatima1@users.noreply.github.com>
2022-11-28 11:27:24 +0000

    file test
    testing if the content I wrote in php displays to other members

---
+++ Website/resources/views/pages/contactus/contact.blade.php
@@ -0,0 +1,39 @@
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- Import main -->
+  <link rel="stylesheet" href="/Website/public/css/contact.css">
+  @include('assets.main')
+
+  <title>@yield('title')</title>
+  <link rel="stylesheet" type="text/css" href="/css/contact.css">
+
+
+</head>
+
+<html>
+
+<body>
+
+  <!-- Import header -->
+  @include('assets.common.navbar')
+
+  
+
+  <!-- Insert ContactUs body here -->
+
+  <div class= "location">
+    
+</div>
+
+
+
+
+  <div class="container-2">
+    <!-- Import footer -->
+    @include('assets.common.footer')
+  </div>
+</body>
+
+</html>
