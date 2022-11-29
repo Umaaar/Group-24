c19b333850f00800d5a40aa9aec564bff211e57f
Rehan Hussain <rehanhh285@gmail.com>
2022-11-29 21:41:55 +0000

    format code

--- Website/resources/views/pages/basket.blade.php
+++ Website/resources/views/pages/basket.blade.php
@@ -5,74 +5,75 @@
 @include('assets.common.navbar')
 
 <head>
-  <title>@yield('title')</title>
+    <title>@yield('title')</title>
 
- 
+
 
 </head>
 
-<?php $total = 0; 
-$decoded = json_decode(json_encode($basket), true)
+<?php $total = 0;
+$decoded = json_decode(json_encode($basket), true);
 ?>
 
 <body>
 
     <!-- Insert basket view body here -->
     <div class="px-3 py-3 pt-md-2.5 pb-md-4 mx-auto text-center">
-      <h1 class="display-6">Basket</h1>
+        <h1 class="display-6">Basket</h1>
     </div>
     <div class="container-fluid d-fluid mx-auto my-4 my-auto col-lg-8">
-    <table class="table table-bordered  w-1">
-      <thead class="thead-primary  btn-primary table-sm table_col_padding">
-        <tr>
-          <th scope="col">Product</th>
-          <th scope="col">Price</th>
-          <th scope="col">Qty</th>
-          <th scope="col">Ops</th>
-        </tr>
-      </thead>
-      <tbody>
-        @foreach ($decoded as $item)
-        <?php $total += $item['totalprice'] * $item['quantity']?>
-        <tr>
-        <form action="removeFromBasket" method="POST">
-          @csrf
-          <td class="table-primary">{{$item['name']}}</td>
-          <td class="table-primary">${{ $item['totalprice'] }}</td>
-          <td class="table-primary">{{ $item['quantity'] }}</td>
-          <td class="table-primary"><button type="submit" class="btn btn-danger">Remove</button></td>
-          <input type="hidden" name="basketcontentsid" value="{{$item['basketcontentsid']}}">
-        </form>
-        </tr>
-        @endforeach
-      </tbody>
-    </table>
-    
-    
-    <div class="row">
-      <div class="col-sm-4 alert alert-primary mx-3">
-    <span>
-        <strong>Total: ${{ $total}}</strong>
-    </span>
-      </div>
-      <div class="col-sm-4">
-        <a href="{{ asset('/checkout') }}" class="btn btn-primary">Checkout</a>
-      </div>
+        <table class="table table-bordered  w-1">
+            <thead class="thead-primary  btn-primary table-sm table_col_padding">
+                <tr>
+                    <th scope="col">Product</th>
+                    <th scope="col">Price</th>
+                    <th scope="col">Qty</th>
+                    <th scope="col">Ops</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($decoded as $item)
+                    <?php $total += $item['totalprice'] * $item['quantity']; ?>
+                    <tr>
+                        <form action="removeFromBasket" method="POST">
+                            @csrf
+                            <td class="table-primary">{{ $item['name'] }}</td>
+                            <td class="table-primary">${{ $item['totalprice'] }}</td>
+                            <td class="table-primary">{{ $item['quantity'] }}</td>
+                            <td class="table-primary"><button type="submit" class="btn btn-danger">Remove</button></td>
+                            <input type="hidden" name="basketcontentsid" value="{{ $item['basketcontentsid'] }}">
+                        </form>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+
+
+        <div class="row">
+            <div class="col-sm-4 alert alert-primary mx-3">
+                <span>
+                    <strong>Total: ${{ $total }}</strong>
+                </span>
+            </div>
+            <div class="col-sm-4">
+                <a href="{{ asset('/checkout') }}" class="btn btn-primary">Checkout</a>
+            </div>
+        </div>
     </div>
-    </div>
-
-    
 
 
 
-                      
-    
 
 
-                  
-         
+
+
+
+
+
+
     <!-- Import footer -->
     @include('assets.common.footer')
 
 </body>
+
 </html>
--- Website/resources/views/pages/checkout.blade.php
+++ Website/resources/views/pages/checkout.blade.php
@@ -1,4 +1,3 @@
-
 <!-- Import main -->
 @include('assets.main')
 
@@ -6,93 +5,98 @@
 @include('assets.common.navbar')
 
 <head>
-  <title>@yield('title')</title>
-  <link rel="stylesheet" href="{{asset('/css/checkout.css')}}">
+    <title>@yield('title')</title>
+    <link rel="stylesheet" href="{{ asset('/css/checkout.css') }}">
 
 </head>
 
 <body>
- 
-  <div class="px-3 py-3 pt-md-2.5 pb-md-4 mx-auto text-center">
-    <h1 class="display-6">Checkout</h1>
-  </div>
+
+    <div class="px-3 py-3 pt-md-2.5 pb-md-4 mx-auto text-center">
+        <h1 class="display-6">Checkout</h1>
+    </div>
     <!-- Insert checkout page here-->
-    
-   <div class="container mt-2 p-3 pt-2">
-    <div class="row">
-      <div class="col-md-7">
-        <div class="card">
-          <div class="card-body">
-            <h6>Basic details</h6>
-            <hr>
-            <div class="row checkout form">
-              <div class="col-md-6">
-               <label for ="">First Name</label>
-               <input type ="text" class= "form-control" placeholder="Enter Your First Name" value="{{ Auth::user()->firstName }}">
-              </div>
-              <div class="col-md-6">
-                <label for ="">Last Name</label>
-                <input type ="text" class= "form-control" placeholder="Enter Your Last Name" value="{{ Auth::user()->surname}}">
-               </div>
-               <div class="col-md-6 mt-3">
-                <label for ="">Email</label>
-                <input type ="text" class= "form-control" placeholder="Enter Your Email" value="{{ Auth::user()->email}}">
-               </div>
-               <div class="col-md-6 mt-3">
-                <label for ="">Address</label>
-                <input type ="text" class= "form-control" placeholder="Enter Your Address"value="{{ Auth::user()->address}}">
-               </div>
-               <div class="col-md-6 mt-3">
-               <label for ="">Post Code</label>
-               <input type ="text" class= "form-control" placeholder="Enter Your Post Code" value="{{ Auth::user()->postCode}}">
-              </div>
+
+    <div class="container mt-2 p-3 pt-2">
+        <div class="row">
+            <div class="col-md-7">
+                <div class="card">
+                    <div class="card-body">
+                        <h6>Basic details</h6>
+                        <hr>
+                        <div class="row checkout form">
+                            <div class="col-md-6">
+                                <label for="">First Name</label>
+                                <input type="text" class="form-control" placeholder="Enter Your First Name"
+                                    value="{{ Auth::user()->firstName }}">
+                            </div>
+                            <div class="col-md-6">
+                                <label for="">Last Name</label>
+                                <input type="text" class="form-control" placeholder="Enter Your Last Name"
+                                    value="{{ Auth::user()->surname }}">
+                            </div>
+                            <div class="col-md-6 mt-3">
+                                <label for="">Email</label>
+                                <input type="text" class="form-control" placeholder="Enter Your Email"
+                                    value="{{ Auth::user()->email }}">
+                            </div>
+                            <div class="col-md-6 mt-3">
+                                <label for="">Address</label>
+                                <input type="text" class="form-control"
+                                    placeholder="Enter Your Address"value="{{ Auth::user()->address }}">
+                            </div>
+                            <div class="col-md-6 mt-3">
+                                <label for="">Post Code</label>
+                                <input type="text" class="form-control" placeholder="Enter Your Post Code"
+                                    value="{{ Auth::user()->postCode }}">
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
+            <div class="col-md-5">
+                <div class="card">
+                    <div class="card-body">
+                        <h6> Order Summary</h6>
+                        <hr>
+                        <table class="table">
+                            <tbody>
+                                <?php
+                                $decoded = json_decode(json_encode($basketInfo), true);
+                                $total = 0;
+                                ?>
+
+                                @foreach ($decoded as $item)
+                                    <?php $total += $item['totalprice'] * $item['quantity']; ?>
+                                    <tr>
+                                        <td>{{ $item['name'] }}</td>
+                                        <td>{{ $item['totalprice'] }}</td>
+                                        <td>{{ $item['quantity'] }}</td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                        <hr>
+                        <div class="row">
+                            <div class="col-md-4">
+                                <h6>Subtotal</h6>
+                            </div>
+                            <div class="col-md-4">
+                                <h6>${{ $total }}</h6>
+                            </div>
+                        </div>
+
+                        <a href="{{ asset('placeOrder') }}"><button class="btn btn-primary">Place Order</button></a>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div> 
-<div class="col-md-5">
-  <div class="card">
-    <div class="card-body">
-     <h6> Order Summary</h6>
-      <hr>
-<table class="table">
-  <tbody>
-    <?php
-      $decoded = json_decode(json_encode($basketInfo), true);
-      $total = 0;
-    ?>
-
-    @foreach ($decoded as $item)
-    <?php $total += $item['totalprice'] * $item['quantity']?>
-    <tr>
-      <td>{{$item['name']}}</td>
-      <td>{{$item['totalprice']}}</td>
-      <td>{{$item['quantity']}}</td>
-    </tr>
-    @endforeach
-  </tbody>
-</table>
-<hr>
-<div class="row">
-  <div class="col-md-4">
-    <h6>Subtotal</h6>
-  </div>
-  <div class="col-md-4">
-    <h6>${{$total}}</h6>
-  </div>
-  </div>
-
-    <a href="{{ asset('placeOrder') }}"><button class="btn btn-primary">Place Order</button></a>
     </div>
-    </div>
-</div>
-    </div>
-   </div>
-     
 
 
 
-  
+
+
 
 
 
@@ -100,4 +104,5 @@
     @include('assets.common.footer')
 
 </body>
+
 </html>
