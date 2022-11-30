5f041f397cf7f6fbb561729c3b76c6ef4a1724f7
Rehan Hussain <rehanhh285@gmail.com>
2022-11-30 10:17:52 +0000

    place order

--- Website/app/Models/Order.php
+++ Website/app/Models/Order.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
   use HasFactory;
-  public $timestamps = true;
+  public $timestamps = false;
   protected $table = 'orders';
 
   protected $fillable = [
